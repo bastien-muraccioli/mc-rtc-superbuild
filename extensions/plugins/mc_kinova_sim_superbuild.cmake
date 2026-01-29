5240d465994bfef162611a392450eda3d9dce1e0
bastien-muraccioli <bastien.muraccioli.pro@gmail.com>
2026-01-29 10:10:42 +0900

    update

--- extensions/plugins/mc_kinova_sim_superbuild.cmake
+++ extensions/plugins/mc_kinova_sim_superbuild.cmake
@@ -4,7 +4,7 @@ AptInstall(libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libglew-dev)
 
 AddProject(mc_mujoco
   GITHUB_PRIVATE bastien-muraccioli/mc_mujoco
-  GIT_TAG origin/safe-rl-qp
+  GIT_TAG origin/main
   CMAKE_ARGS -DMUJOCO_ROOT_DIR=${MUJOCO_ROOT_DIR}
   DEPENDS mc_rtc
 )
