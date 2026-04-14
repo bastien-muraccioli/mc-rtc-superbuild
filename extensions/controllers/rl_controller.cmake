ac57b83b9bb77a45e466f238c6a528618229a9aa
bastien-muraccioli <bastien.muraccioli.pro@gmail.com>
2026-04-14 14:15:28 +0900

    update extensions

--- extensions/controllers/rl_controller.cmake
+++ extensions/controllers/rl_controller.cmake
@@ -1,5 +1,7 @@
 AddProject(rl_controller
   GITHUB_PRIVATE bastien-muraccioli/rl_controller
-  GIT_TAG origin/paper_version
+  # GIT_TAG origin/paper_version
+  # GIT_TAG origin/gravity-compensation
+  GIT_TAG origin/devel
   DEPENDS mc_rtc
 )
