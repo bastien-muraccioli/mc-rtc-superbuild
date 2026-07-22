6c38ea8e9d46fcfd19d72aec89f63f91d4d895d9
ThibaultDel <delooeckerthibault@gmail.com>
2026-07-22 16:01:20 +0900

    change used remote

--- extensions/interfaces/MujocoRosUtils.cmake
+++ extensions/interfaces/MujocoRosUtils.cmake
@@ -1,5 +1,5 @@
 AddProject(MujocoRosUtils
-        GITHUB isri-aist/MujocoRosUtils
+        GITHUB ThibaultDel/MujocoRosUtils
         GIT_TAG origin/main
         CMAKE_ARGS -DMUJOCO_ROOT_DIR="$ENV{HOME}/.mujoco/mujoco-3.3.6"
         DEPENDS mc_mujoco
--- extensions/interfaces/mujoco.cmake
+++ extensions/interfaces/mujoco.cmake
@@ -1,8 +1,8 @@
 AptInstall(libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libglew-dev)
 
 AddProject(mc_mujoco
-	GITHUB bastien-muraccioli/mc_mujoco
-	GIT_TAG bastien-muraccioli/main
+	GITHUB ThibaultDel/mc_mujoco
+	GIT_TAG origin/main
         CMAKE_ARGS
           -DMUJOCO_ROOT_DIR=$ENV{HOME}/.mujoco/mujoco-3.3.6
 )
