d47a7e9ac6dd3d71eacaa1919a31236967717a8f
ThibaultDel <delooeckerthibault@gmail.com>
2026-07-24 11:37:50 +0900

    extensions file location change

--- extensions/local.cmake
+++ extensions/local.cmake
@@ -1,6 +1,9 @@
 set(EXTENSIONS_DIR ${CMAKE_CURRENT_LIST_DIR})
 include(${EXTENSIONS_DIR}/interfaces/mujoco.cmake)
 include(${EXTENSIONS_DIR}/interfaces/MujocoRosUtils.cmake)
-include(${EXTENSIONS_DIR}/plugins/jrl-qp.cmake)
+
 include(${EXTENSIONS_DIR}/plugins/MyPluginPassivityHumanoid.cmake)
 include(${EXTENSIONS_DIR}/controllers/hammeringtasknew.cmake)
+
+include(${EXTENSIONS_DIR}/observers/mc_external_forces_observer.cmake)
+
